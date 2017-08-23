b391b1b5b69dda96c3d31ec079aa203841cc28bf
Magnus Hyttsten <magnus.hyttsten@gmail.com>
2017-08-23 14:16:53 -0700

    RC1

--- Blog_Estimators_DataSet.py
+++ Blog_Estimators_DataSet.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import collections
 import os
 import urllib
+import numpy as np
 import sys
 
 # Check that we have correct TensorFlow version installed
@@ -12,22 +13,20 @@ assert "1.3" <= tf_version, "TensorFlow r1.3 or later is needed"
 # Windows users: You only need to change PATH, rest is platform independent
 PATH = "/tmp/tf_dataset_and_estimator_apis"
 
-# Load Training and Test dataset files
+# Fetch and store Training and Test dataset files
 PATH_DATASET = PATH +os.sep + "dataset"
-FILE_TRAIN =   PATH_DATASET + os.sep + "boston_train.csv"
-FILE_TEST =    PATH_DATASET + os.sep + "boston_test.csv"
-URL_TRAIN =   "http://download.tensorflow.org/data/boston_train.csv"
-URL_TEST =    "http://download.tensorflow.org/data/boston_test.csv"
+FILE_TRAIN =   PATH_DATASET + os.sep + "iris_training.csv"
+FILE_TEST =    PATH_DATASET + os.sep + "iris_test.csv"
+URL_TRAIN =   "http://download.tensorflow.org/data/iris_training.csv"
+URL_TEST =    "http://download.tensorflow.org/data/iris_test.csv"
 def downloadDataset(url, file):
     if not os.path.exists(PATH_DATASET):
         os.makedirs(PATH_DATASET)
-
     if not os.path.exists(file):
         data = urllib.urlopen(url).read()
         with open(file, "w") as f:
             f.write(data)
             f.close()
-
 downloadDataset(URL_TRAIN, FILE_TRAIN)
 downloadDataset(URL_TEST, FILE_TEST)
 
@@ -35,16 +34,11 @@ tf.logging.set_verbosity(tf.logging.INFO)
 
 # The CSV fields in the files
 dataset_fields = collections.OrderedDict([
-    ('CrimeRate',           [0.]),
-    ('LargeLotsRate',       [0.]),
-    ('NonRetailBusRate',    [0.]),
-    ('NitricOxidesRate',    [0.]),
-    ('RoomsPerHouse',       [0.]),
-    ('Older1940Rate',       [0.]),
-    ('Dist2EmployeeCntr',   [0.]),
-    ('PropertyTax',         [0.]),
-    ('StudentTeacherRatio', [0.]),
-    ('MarketValueIn10k',    [0.])
+    ('SepalLength',    [0.]), # tf.float32
+    ('SepalWidth',     [0.]), # tf.float32
+    ('PetalLength',    [0.]), # tf.float32
+    ('PetalWidth',     [0.]), # tf.float32
+    ('IrisFlowerType', [0])   # tf.int32
 ])
 
 # Create an input function reading a file using the Dataset API
@@ -58,28 +52,28 @@ def my_input_fn(file_path, repeat_count):
         tf.contrib.data.TextLineDataset(file_path) # Read text line file
             .skip(1) # Skip header row
             .map(decode_csv) # Transform each elem by applying decode_csv fn
-            .shuffle(buffer_size=1000) # Obs: buffer_size is read into memory
+            .shuffle(buffer_size=256) # Obs: buffer_size is read into memory
             .repeat(repeat_count) #
-            .batch(128)) # Batch size to use
+            .batch(32)) # Batch size to use
     iterator = dataset.make_one_shot_iterator()
     batch_features = iterator.get_next()
-    batch_labels = batch_features.pop('MarketValueIn10k')
-    return batch_features,\
-           batch_labels
+    batch_labels = batch_features.pop('IrisFlowerType')
+    return batch_features, batch_labels
 
-feature_names = dataset_fields.copy() # Create a list of our input features
-del feature_names['MarketValueIn10k'] # Remove the label field
 
 # Create the feature_columns, which specifies the input to our model
 # All our input features are numeric, so use numeric_column for each one
+feature_names = dataset_fields.copy() # Create a list of our input features
+del feature_names['IrisFlowerType'] # Remove the label field
 feature_columns = [tf.feature_column.numeric_column(k) for k in feature_names]
 
 # Create a deep neural network regression classifier
 # Use the DNNRegressor canned estimator
-classifier = tf.estimator.DNNRegressor(
+classifier = tf.estimator.DNNClassifier(
     feature_columns=feature_columns, # The input features to our model
     hidden_units=[10, 10], # Two layers, each with 10 neurons
-    model_dir=PATH) # Checkpoints etc are stored here
+    n_classes=3,
+    model_dir=PATH) # Path to where checkpoints etc are stored
 
 # Train our model, use the previously function my_input_fn
 # Input to training is a file with training example
@@ -97,9 +91,31 @@ print("Evaluation results")
 for key in evaluate_result:
     print("   {}, was: {}".format(key, evaluate_result[key]))
 
-# Model evaluated, now use it to predict some house prices
-# Let's predict the examples in FILE_TEST
-predict_result = classifier.predict(
-    input_fn=lambda: my_input_fn(FILE_TEST, 1))
-for x in predict_result:
-    print x
+# Let create a dataset for prediction
+# We've taken the first 3 examples in FILE_TEST
+prediction_input = [[5.9, 3.0, 4.2, 1.5],  # -> 1, Iris Versicolor
+                    [6.9, 3.1, 5.4, 2.1],  # -> 2, Iris Virginica
+                    [5.1, 3.3, 1.7, 0.5]]  # -> 0, Iris Sentosa
+def new_input_fn():
+    def decode(x):
+        x = tf.split(x, 4) # Need to split into our 4 features
+        return dict(zip(feature_names, x)) # To build a dict of them
+
+    dataset = tf.contrib.data.Dataset.from_tensor_slices(prediction_input)
+    dataset = dataset.map(decode)
+    iterator = dataset.make_one_shot_iterator()
+    next_feature_batch = iterator.get_next()
+    return next_feature_batch, None # In prediction, we have no labels
+
+# Predict all our prediction_input
+predict_results = classifier.predict(input_fn=new_input_fn)
+
+# Print results
+for idx, prediction in enumerate(predict_results):
+    type = prediction["class_ids"][0] # Get the predicted class (index)
+    if type == 0:
+        print("I think: {}, is Iris Sentosa".format(prediction_input[idx]))
+    elif type == 1:
+        print("I think: {}, is Iris Versicolor".format(prediction_input[idx]))
+    else:
+        print("I think: {}, is Iris Virginica".format(prediction_input[idx]))
