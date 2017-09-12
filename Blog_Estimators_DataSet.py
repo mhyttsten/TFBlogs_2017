f813dcf2fdd06027aff9a97d3ae1d0b327a07971
Magnus Hyttsten <magnus.hyttsten@gmail.com>
2017-09-12 08:55:13 -0700

    Initial Release

--- Blog_Estimators_DataSet.py
+++ Blog_Estimators_DataSet.py
@@ -1,9 +1,25 @@
+# Copyright 2017 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+# This is the complete code for the following blogpost:
+# https://developers.googleblog.com/2017/09/introducing-tensorflow-datasets.html
+#   (https://goo.gl/Ujm2Ep)
+
 import tensorflow as tf
-import collections
 import os
 import urllib
-import numpy as np
-import sys
 
 # Check that we have correct TensorFlow version installed
 tf_version = tf.__version__
